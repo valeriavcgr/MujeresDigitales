b74bc2cd20db093bf090c86da0e72a0d054204fd
valeriavcgr <valeria3025gon@gmail.com>
2025-09-04 18:15:06 -0500

    tablas de multiplicar con javascript

---
+++ Tablas.js
@@ -0,0 +1,8 @@
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`Tabla del ${i}:`);
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${base} x ${i} = ${base * i}`);
+  }
+  console.log(''); // línea en blanco entre tablas
+}

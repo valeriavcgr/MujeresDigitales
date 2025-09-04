09652e24964a253e79e19c622b5df935a47770bf
valeriavcgr <valeria3025gon@gmail.com>
2025-09-04 18:53:57 -0500

    Update Tablas.js

--- Tablas.js
+++ Tablas.js
@@ -2,6 +2,6 @@
 for (let i = 1; i <= 10; i++) {
   console.log(`Tabla del ${i}:`);
   for (let i = 1; i <= 10; i++) {
-    console.log(`${base} x ${i} = ${base * i}`);
+    console.log(`${base} * ${i} = ${base * i}`);
   }
 }

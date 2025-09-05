c6604f8cfd5e8c2650b24072aa66bc39a6026e35
valeriavcgr <valeria3025gon@gmail.com>
2025-09-04 20:48:55 -0500

    Update Tablas.js

--- Tablas.js
+++ Tablas.js
@@ -1,7 +1,7 @@
 //Tablas de multiplicar del 1 al 10
-for (let i = 1; i <= 10; i++) {
-  console.log(`Tabla del ${i}:`);
+for (let num = 1; num <= 10; num++) {
+  console.log(`Tabla del ${num}:`);
   for (let i = 1; i <= 10; i++) {
-    console.log(`${base} * ${i} = ${base * i}`);
+    console.log(`${num} * ${i} = ${num * i}`);
   }
 }

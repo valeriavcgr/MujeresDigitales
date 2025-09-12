a18a39fa66e54fa3ab439c1cf4a9798ccca6f450
valeriavcgr <valeria3025gon@gmail.com>
2025-09-12 17:32:49 -0500

    mejora de tablas multiplicar

--- Ejercicios/Tablas.js
+++ Ejercicios/Tablas.js
@@ -1,4 +1,4 @@
-
+//Tablas de multiplicar del 1 al 10
 for (let num = 1; i <= 10; num++) {
 
   console.log(`Tabla del ${num}`);
--- Tablas.js
+++
@@ -1,7 +0,0 @@
-//Tablas de multiplicar del 1 al 10
-for (let num = 1; num <= 10; num++) {
-  console.log(`Tabla del ${num}:`);
-  for (let i = 1; i <= 10; i++) {
-    console.log(`${num} * ${i} = ${num * i}`);
-  }
-}

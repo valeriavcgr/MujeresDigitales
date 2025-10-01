f177d0d6ad114a0ca67517fc4753cc1a4de11757
valeriavcgr <valeria3025gon@gmail.com>
2025-09-30 20:31:56 -0500

    dto productos- configurar la concección a bd

--- appi1/src/app.module.ts
+++ appi1/src/app.module.ts
@@ -1,13 +1,35 @@
 import { Module } from '@nestjs/common';
-import {ConfigModule} from '@nestjs/config';            
+import {ConfigModule, ConfigService} from '@nestjs/config';            
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+// impotamos el modulo de configuracion para las variables de entorno
+// importamos el modulo de typeorm para la conexion a la base de datos
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}),UsersModule, ProductsModule, AuthModule],
+  imports: [
+  ConfigModule.forRoot({isGlobal: true}),
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => ({
+      type: 'mysql',
+      host: config.get('DB_HOST'),
+      port: config.get('DB_PORT'),
+      username: config.get('DB_USERNAME'),
+      password: config.get('DB_PASSWORD'),
+      database: config.get('DB_NAME'),
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  }),
+  UsersModule,
+  ProductsModule, 
+  AuthModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
--- appi1/src/dto/create-product.dto.ts
+++ appi1/src/dto/create-product.dto.ts
@@ -1,9 +1,15 @@
+import { IsNotEmpty } from "class-validator";
+
 export type IProduct ={id:number, name: string, price:number, stock:number, description:string}
 
 export class CreateProductDTO{
+    @IsNotEmpty()
     name: string;
+    @IsNotEmpty()
     price:number
+    @IsNotEmpty()
     stock:number
+    @IsNotEmpty()
     description:string
 
 }
